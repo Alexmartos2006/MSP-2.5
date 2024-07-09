34437218980d94c1e73ecd49316a8273d6ea91e1
Alex <164188226+Alexmartos2006@users.noreply.github.com>
2024-07-09 15:00:10 +0000

    Mundo guardado 2024-07-09

--- mcbot/index.js
+++ mcbot/index.js
@@ -1,5 +1,9 @@
 const mineflayer = require('mineflayer'); // importar el módulo de mineflayer.
 const prefix = '!'; // activar el comando con el prefijo '!'
+const { mineflayer: mineflayerViewer } = require('prismarine-viewer') // importar el modulo mineflayerViewer
+
+
+
 
 const bot = mineflayer.createBot({  // creación del bot.
   host: '100.87.107.68', // minecraft server ip
@@ -9,6 +13,16 @@ const bot = mineflayer.createBot({  // creación del bot.
   version: "1.18.2",           // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
   // password: '12345678'      // set if you want to use password-based auth (may be unreliable). If specified, the `username` must be an email
 });
+
+
+
+bot.once('spawn', () => {
+  mineflayerViewer(bot, { port: 3007, firstPerson: true }) // El puerto es el puerto del servidor de Minecraft. Si la primera persona es falsa, obtendrás una vista panorámica.
+})
+
+
+
+
 
 bot.on('chat', (username, message) => {
   if (username == bot.username) return
@@ -22,9 +36,12 @@ bot.on('chat', (username, message) => {
   if(command == 'di') {
       bot.chat(args.join(' '));
   }
-  
+  bot.
 
 });
+
+
+
 
 // Muestra los errores en la consola o motivos de explusación:
 bot.on('kicked', console.log);
